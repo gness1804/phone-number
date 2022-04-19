2a27d403b5df96b3cb65091db447a72675137bd0
gness1804 <graham.nessler@gmail.com>
2022-04-19 15:26:20 -0500

    next few tests

--- phone-number.ts
+++ phone-number.ts
@@ -3,7 +3,11 @@ const removeInvalidChars = (str: string): string => str
   .replace(/\)/g, '')
   .replace(/\s/g, '')
   .replace(/-/g, '')
+  .replace(/\./g, '')
 
-export function clean(num: string): string {
-  return removeInvalidChars(num);
+export function clean(numStr: string): string {
+  if (numStr.length !== 10) {
+    throw new Error('Incorrect number of digits')
+  }
+  return removeInvalidChars(numStr);
 }
