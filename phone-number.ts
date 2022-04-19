ae6af94d7f2acfe8f2ce15371d07d3a0b6bdf2d0
gness1804 <graham.nessler@gmail.com>
2022-04-19 15:35:42 -0500

    move around logic

--- phone-number.ts
+++ phone-number.ts
@@ -4,10 +4,16 @@ const removeInvalidChars = (str: string): string => str
   .replace(/\s/g, '')
   .replace(/-/g, '')
   .replace(/\./g, '')
+  .replace(/^\+1/g, '')
 
 export function clean(numStr: string): string {
-  if (numStr.length !== 10) {
+  const sanitizedStr = removeInvalidChars(numStr);
+  if (sanitizedStr.length === 11) {
+    if (!sanitizedStr.startsWith('1')) {
+      throw new Error('11 digits must start with 1')
+    }
+  } else if (sanitizedStr.length !== 10) {
     throw new Error('Incorrect number of digits')
   }
-  return removeInvalidChars(numStr);
+  return sanitizedStr;
 }
